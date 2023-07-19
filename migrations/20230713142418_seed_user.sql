37ac3bc79d2557b8c587c997d1176a489fc25fb5
abhicte <abhichtechfp@gmail.com>
2023-07-19 12:56:44 +0530

    auth,redis,idempotency

---
+++ migrations/20230713142418_seed_user.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+'760f4716-0301-4518-85fa-b244438ec93e',
+'admin',
+'$argon2id$v=19$m=15000,t=2,p=1$VENj8PbDF3RhNozgOwTMyA$9l3LBBLFCRqTIecEC+KSnTzXn0WfA2B5sHUI+xVWHCg');
+
+
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,31 +1,23 @@
+use actix_web::rt::task::JoinHandle;
 use tracing::subscriber::set_global_default;
 use tracing::Subscriber;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+
 /// Compose multiple layers into a `tracing`'s subscriber.
 ///
 /// # Implementation Notes
 ///
-/// We are using `impl Subscriber` as return type to avoid having to
-/// spell out the actual type of the returned subscriber, which is
-/// indeed quite complex.
-/// We need to explicitly call out that the returned subscriber is
-/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
-/// later on.
+/// We are using `impl Subscriber` as return type to avoid having to spell out the actual
+/// type of the returned subscriber, which is indeed quite complex.
 pub fn get_subscriber<Sink>(
     name: String,
     env_filter: String,
     sink: Sink,
-) -> impl Subscriber + Send + Sync
+) -> impl Subscriber + Sync + Send
 where
-    // This "weird" syntax is a higher-ranked trait bound (HRTB)
-    // 120 CHAPTER 4. TELEMETRY
-    // It basically means that Sink implements the `MakeWriter`
-    // trait for all choices of the lifetime parameter `'a`
-    // Check out https://doc.rust-lang.org/nomicon/hrtb.html
-    // for more details.
     Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
 {
     let env_filter =
@@ -36,9 +28,20 @@ where
         .with(JsonStorageLayer)
         .with(formatting_layer)
 }
+
 /// Register a subscriber as global default to process span data.
+///
 /// It should only be called once!
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+pub fn init_subscriber(subscriber: impl Subscriber + Sync + Send) {
     LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    let current_span = tracing::Span::current();
+    actix_web::rt::task::spawn_blocking(move || current_span.in_scope(f))
 }
